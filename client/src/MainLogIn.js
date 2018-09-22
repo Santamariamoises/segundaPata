001b69cf673053950cd67264ae57fd8189ec67d9
FernandoFfigueroa0710 <fernandoffigueroa@gmail.com>
2018-09-22 09:53:27 -0500

    update- Log in and out features working;

---
+++ client/src/MainLogIn.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import fire from './components/Fire.js';
+import MainPage from './MainPage';
+import Login from './components/logInComponent/Login';
+
+class MainLogin extends Component {
+  constructor() {
+    super();
+    this.state = ({
+      user: null,
+    });
+    this.authListener = this.authListener.bind(this);
+  }
+
+  componentDidMount() {
+    this.authListener();
+  }
+
+  authListener() {
+    fire.auth().onAuthStateChanged((user) => {
+      console.log(user);
+      if (user) {
+        this.setState({ user });
+        localStorage.setItem('user', user.uid);
+      } else {
+        this.setState({ user: null });
+        localStorage.removeItem('user');
+      }
+    });
+  }
+  render() {
+    return (
+     <div>{this.state.user ? ( <MainPage />) : (<Login />)}</div>
+)};
+};
+
+ export default MainLogin;
---
+++ client/src/MainPage.jsx
@@ -0,0 +1,105 @@
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+import $ from "jquery";
+import axios from "axios";
+import fire from "./components/Fire.js"
+
+import SearchBar from "./components/search/SearchBar.js";
+import ItemPage from "./components/item-page/ItemPage.jsx";
+import ItemsHome from "./components/ItemsHome.js";
+import UploadProduct from "./components/UploadProduct.jsx";
+import Footer from "./components/item-page/footer.js";
+
+class MainPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      products: undefined,
+      product: undefined
+    }
+    this.setProduct = this.setProduct.bind(this);
+    this.handleSubmitToys = this.handleSubmitToys.bind(this);
+    this.handleSubmitClothes = this.handleSubmitClothes.bind(this);
+    this.handleSubmitBeds = this.handleSubmitBeds.bind(this);
+    this.handleSubmitAccesories = this.handleSubmitAccesories.bind(this);
+  }
+
+  componentDidMount() {
+    axios.get('/items').then(res => {
+      const items = res.data;
+      this.setState({items});
+    })
+  }
+
+  handleSubmitToys() {
+    //alert('toys called');
+    axios.get('/toys').then(res => {
+      console.log(res);
+      var products = res.data;
+      this.setState({products});
+    })
+  }
+
+  handleSubmitClothes() {
+    //alert('toys called');
+    axios.get('/clothes').then(res => {
+      console.log(res);
+      var products = res.data;
+      this.setState({products});
+    })
+  }
+  handleSubmitBeds() {
+    //alert('toys called');
+    axios.get('/beds').then(res => {
+      console.log(res);
+      var products = res.data;
+      this.setState({products});
+    })
+  }
+
+  handleSubmitAccesories() {
+    //alert('toys called');
+    axios.get('/accesories').then(res => {
+      console.log(res);
+      var products = res.data;
+      this.setState({products});
+    })
+  }
+
+  setProduct(id) {
+    this.setState({product: id})
+  }
+
+  // postItem(Upload){
+  //   axios.post('item', {
+  //   newUpload = {
+  //     name: "",
+  //     description:"",
+  //     price:"",
+  //     category:"",
+  //     email:"",
+  //     vendor:""
+  //
+  //   }
+  // })
+  // }
+
+  render() {
+    return (<div>
+      <center>
+        <h1>
+          <img src="https://i.imgur.com/hxDOW9A.jpg" title="Una pagina para tus mascotas!"/>
+          <div>Segunda Pata</div>
+        </h1>
+        <h2>Vende y Compra Accesorios, para Mascotas.</h2>
+        <SearchBar items={this.state.items} handleSubmitToys={this.handleSubmitToys} handleSubmitClothes={this.handleSubmitClothes} handleSubmitBeds={this.handleSubmitBeds} handleSubmitAccesories={this.handleSubmitAccesories}/>
+        <ItemsHome items={this.state.items} products={this.state.products} setProduct={this.setProduct}/>
+      </center>
+      <div className="fb-like" data-href="https://github.com/mambo-num-5" data-layout="standard" data-action="like" data-size="large" data-show-faces="true" data-share="true"></div>
+      <Footer/>
+    </div>)
+  }
+};
+
+export default MainPage;
